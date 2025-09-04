16c600717d1393830fa5666eb2e9399847663c6a
mehmetdora333 <mehmetdora333@gmail.com>
2025-09-04 07:28:09 +0000

    System generated commit, initial commit

---
+++ pages/Yeni Kayıt/jsobjects/arabalar_crud/arabalar_crud.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	
+	async create_araba (marka,model,yil) {
+		
+		
+		console.log(marka,model,yil);
+		create_araba_query.run({marka,model,yil});
+		
+		
+		
+	},
+	
+	
+	
+}
